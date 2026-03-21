d1de71ea0fedf76854d53b24130be5c9c69a4afe
Alfredo <amancio.mota07@gmail.com>
2026-03-21 17:06:24 -0300

    Exercicio 1003 feito em C++

---
+++ Iniciante/1003/somaSimples.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+    int numero1;
+    int numero2;
+    int soma;
+    std::cin >> numero1;
+    std::cin >> numero2;
+    soma = numero1+numero2;
+    std::cout << "SOMA = " << soma <<"\n";
+    return 0;
+}
